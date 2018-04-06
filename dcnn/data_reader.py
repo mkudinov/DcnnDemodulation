69fc3a7465ab55c0eb984267b1d61fdec884a0b4
mkudinov <mikhailkudinov@gmail.com>
2018-04-07 00:47:00 +0300

    initial

---
+++ dcnn/data_reader.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class SignalGenerator(object):
+    """Class for FSK signals generation with AWGN and Reileigh fading
+    SNR can be specified for each signal."""
+
+    def __init__(self):
+        pass
