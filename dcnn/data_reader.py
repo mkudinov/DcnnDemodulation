90637d889cf8b56e5078b95d0e139e1db1e80303
mkudinov <mikhailkudinov@gmail.com>
2018-04-09 01:02:11 +0300

    Data generator refactored. TF dataset source is started

--- dcnn/data_reader.py
+++ dcnn/data_reader.py
@@ -12,34 +12,38 @@ class SignalGeneratorASCII(object):
        see "Demodulation of Faded Wireless Signals using Deep Convolutional Neural Networks"
        http://sce2.umkc.edu/csee/beardc/DCNN%20Demodulation%20UMKC%20CCWC18.pdf
     """
-
-    def __init__(self, path_to_text, frequency_mark=984.0, frequency_space=966.0, baud_rate=2, sample_rate=10**6):
+    def __init__(self, path_to_text, frequency_mark=984.0, frequency_space=966.0, bit_rate=2, sample_rate=10 ** 6):
         """
-        :param path_to_text (string): path to the text file used for signal generation
-        :param frequency_mark (float32): frequency corresponding to logical "1" BFSK
-        :param frequency_space (float32): frequency corresponding to logical "0" BFSK
-        :param baud_rate (int): baud rate
-        :param sample_rate (int): sample rate
+        :param path_to_text: path to the text file used for signal generation
+        :type path_to_text: string
+        :param frequency_mark: frequency corresponding to logical "1" BFSK
+        :type frequency_mark: float32
+        :param frequency_space: frequency corresponding to logical "0" BFSK
+        :type frequency_space: float32
+        :param bit_rate: baud rate = bit rate
+        :type  bit_rate: int
+        :param sample_rate: sample rate
+        :type sample_rate: int
         """
-        self.frequency_mark = frequency_mark
-        self.frequency_space = frequency_space
-        self.baud_rate = baud_rate
-        self.sample_rate = sample_rate
-        bit_time = np.linspace(0.0, 1.0 / self.baud_rate, int(self.sample_rate / self.baud_rate))
-        self.frequencies = [np.sin(2 * np.pi * self.frequency_space * bit_time),
-                            np.sin(2 * np.pi * self.frequency_mark * bit_time)]
-        self.message = self._load_text(path_to_text)
-        self.subsampling_factor = 100  # WARNING: MAGIC NUMBER FROM PAPER
+        self._frequency_mark = frequency_mark
+        self._frequency_space = frequency_space
+        self._bit_rate = bit_rate
+        self._sample_rate = sample_rate
+        bit_time = np.linspace(0.0, 1.0 / self._bit_rate, int(self._sample_rate / self._bit_rate))
+        self._frequencies = [np.sin(2 * np.pi * self._frequency_space * bit_time),
+                             np.sin(2 * np.pi * self._frequency_mark * bit_time)]
+        self._message = self._load_text(path_to_text)
+        self._samples_per_bit = self._bit_rate / self._sample_rate  # WARNING: MAGIC NUMBER FROM PAPER
 
     def _load_text(self, path_to_text):
         full_message = []
         for line in open(path_to_text):
             # 1000 msec pause before signal start
-            full_message += [0] * self.baud_rate
+            full_message += [0] * self._bit_rate
             for letter in line.strip():
                 full_message.append(ord(letter))
             # 1000 msec pause after signal start
-            full_message += [0] * self.baud_rate
+            full_message += [0] * self._bit_rate
         return full_message
 
     def _generate_fsk_byte_frames(self, number):
@@ -49,13 +53,43 @@ class SignalGeneratorASCII(object):
         for digit in binary_form:
             digit = int(digit)
             bit_sequence.append(digit)
-            signal_fsk.append(self.frequencies[digit])
+            signal_fsk.append(self._frequencies[digit])
         return signal_fsk, bit_sequence
 
-    def _subsample(self, bit_frame):
-        return np.random.choice(bit_frame, self.subsampling_factor)
-
     def __next__(self):
-        for ascii_code in self.message:
+        for ascii_code in self._message:
             for bit_frame, bit in self._generate_fsk_byte_frames(ascii_code):
-                yield bit, self._subsample(bit_frame)
+                yield bit, bit_frame
+
+
+class AwgnParameterGenerator(object):
+    """
+    Helper class for adding noise with target SNR values
+    """
+    def __init__(self, snr_values):
+        """
+        :param snr_values: target SNR values for noise generation
+        :type snr_values: list of float32
+        """
+        self.snr_values = snr_values
+
+    def random_noise_parameter(self):
+        """
+        Get SNR for the next noisy signal frame
+        :return: SNR value
+        """
+        return np.random.choice(self.snr_values, 1)[0]
+
+
+class TfSignalSource(object):
+    """
+    Class is intended to transfer signal data in the form of Numpy arrays to tesorflow
+    """
+    def __init__(self, signal_generator, noise_generator):
+        """
+        :param signal_generator: signal generator
+        :type signal_generator: SignalGeneratorASCII
+        :param noise_generator: object for white noise generation
+        :type noise_generator: AwgnGenerator
+        """
+        self._generator = signal_generator
