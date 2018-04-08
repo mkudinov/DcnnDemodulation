de6afa44c360b15e163b63e9896cd973c684aec3
mkudinov <mikhailkudinov@gmail.com>
2018-04-09 01:21:08 +0300

    rename main>train

--- train.py
+++ train.py
@@ -14,13 +14,4 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    sess = tf.Session()
-    vgg = DCNN()
-
-    img1 = imread('laska.png', mode='RGB')
-    img1 = imresize(img1, (224, 224))
-
-    prob = sess.run(vgg.probs, feed_dict={vgg.imgs: [img1]})[0]
-    preds = (np.argsort(prob)[::-1])[0:5]
-    for p in preds:
-        print class_names[p], prob[p]
+    SNR_levels = np.arange(10.0, 20.0, 2.0, np.float32)
