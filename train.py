ee060ef6d808940f216805865612b8e3e3ae575f
mkudinov <mikhailkudinov@gmail.com>
2018-04-10 01:07:19 +0300

    training with dataset api is started

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 __author__ = 'm.kudinov'
 
-
 ########################################################################################
 # Mikhail Kudinov, 2018                                                                #
 # Demodulation of Faded Wireless Signals using Deep Convolutional Neural Networks      #
@@ -11,7 +10,18 @@ __author__ = 'm.kudinov'
 
 import tensorflow as tf
 import numpy as np
-
+from dcnn.data_reader import *
 
 if __name__ == '__main__':
     SNR_levels = np.arange(10.0, 20.0, 2.0, np.float32)
+    signal_generator = SignalGeneratorASCII("train_text.txt")
+    training_set = None
+    for snr_level in SNR_levels:
+        if training_set is not None:
+            training_set.concatenate(
+                tf.data.Dataset.from_generator(signal_generator, signal_generator.output_types).map(
+                    lambda x, y: add_noise_and_fft(x, y, snr_level)))
+        else:
+            training_set = tf.data.Dataset.from_generator(signal_generator, signal_generator.output_types).map(
+                lambda x, y: add_noise_and_fft(x, y, snr_level))
+    training_set.shuffle(10000, reshuffle_each_iteration=True)
