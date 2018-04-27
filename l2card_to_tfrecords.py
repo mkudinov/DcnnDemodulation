43a10eaa51e4bf251c77c002b0587780263ce204
mkudinov <mikhailkudinov@gmail.com>
2018-04-27 13:10:35 +0300

    tiny correction to file load

--- l2card_to_tfrecords.py
+++ l2card_to_tfrecords.py
@@ -8,5 +8,8 @@ if __name__ == '__main__':
     onlyfiles = {os.path.join(path_to_signals, f)[:-4] for f in os.listdir(path_to_signals) if os.path.isfile(os.path.join(path_to_signals, f))}
     for file in onlyfiles:
         binary_signal_file = file + '.dat'
+        print (binary_signal_file)
+        signal = np.fromfile(binary_signal_file)
+
 
 
